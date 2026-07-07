10a47e4338080225c19cb15d687ded730068befb
lee-y.ch <lee-y.ch@lee-ychui-MacBookPro.local>
2026-07-07 16:51:23 +0900

    chore: promote community backend to repository root

---
+++ Week_4/community/build/generated/sources/annotationProcessor/java/main/com/community/community/entity/QUser.java
@@ -0,0 +1,45 @@
+package com.community.community.entity;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+
+
+/**
+ * QUser is a Querydsl query type for User
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QUser extends EntityPathBase<User> {
+
+    private static final long serialVersionUID = 1874503647L;
+
+    public static final QUser user = new QUser("user");
+
+    public final StringPath email = createString("email");
+
+    public final StringPath nickname = createString("nickname");
+
+    public final StringPath password = createString("password");
+
+    public final StringPath profileImageUrl = createString("profileImageUrl");
+
+    public final NumberPath<Integer> userId = createNumber("userId", Integer.class);
+
+    public QUser(String variable) {
+        super(User.class, forVariable(variable));
+    }
+
+    public QUser(Path<? extends User> path) {
+        super(path.getType(), path.getMetadata());
+    }
+
+    public QUser(PathMetadata metadata) {
+        super(User.class, metadata);
+    }
+
+}
+
